3a49506908d9ecf073bccbc5d129351cab79efe7
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2021-11-03 05:57:24 +0600

    again testing commit

--- app.js
+++ app.js
@@ -1,1 +1,99 @@
-const startGameBtn = document.getElementById('start-game-btn');
+// const names = ['Hole-in-one!', 'Eagle', 'Birdie', 'Par', 'Bogey', 'Double Bogey', 'Go Home!'];
+
+// function golfScore(par, strokes) {
+//     // Only change code below this line
+//     if (strokes === 1) {
+//         return names[0];
+//     }
+//     if (strokes <= par - 2) {
+//         return names[1];
+//     }
+//     if (strokes === par - 1) {
+//         return names[2];
+//     }
+//     if (strokes === par) {
+//         return names[3];
+//     }
+//     if (strokes === par + 1) {
+//         return names[4];
+//     }
+//     if (strokes === par + 2) {
+//         return names[5];
+//     }
+//     if (strokes >= par + 3) {
+//         return names[6];
+//     }
+//     return 'Change Me';
+//     // Only change code above this line
+// }
+
+// golfScore(5, 4);
+// function caseInSwitch(val) {
+//     let answer = '';
+//     // Only change code below this line
+//     switch (val) {
+//         case 1:
+//             answer = 'alpha';
+//             break;
+//         case 2:
+//             answer = 'beta';
+//             break;
+//         case 3:
+//             answer = 'gamma';
+//             break;
+//         case 4:
+//             answer = 'delta';
+//             break;
+//         default:
+//             answer = '';
+//     }
+
+//     // Only change code above this line
+//     return answer;
+// }
+
+// caseInSwitch(1);
+
+// function chainToSwitch(val) {
+//     let answer = '';
+//     // Only change code below this line
+
+//     // if (val === 'bob') {
+//     //     answer = 'Marley';
+//     // } else if (val === 42) {
+//     //     answer = 'The Answer';
+//     // } else if (val === 1) {
+//     //     answer = 'There is no #1';
+//     // } else if (val === 99) {
+//     //     answer = 'Missed me by this much!';
+//     // } else if (val === 7) {
+//     //     answer = 'Ate Nine';
+//     // }
+
+//     switch (val) {
+//         case 'bob':
+//             answer = 'Marley';
+//             break;
+//         case 42:
+//             answer = 'The Answer';
+//             break;
+//         case 1:
+//             answer = 'There is no #1';
+//             break;
+//         case 99:
+//             answer = 'Missed me by this much!';
+//             break;
+//         case 7:
+//             answer = 'Ate Nine';
+//             break;
+//         default:
+//             answer = '';
+//             break;
+//     }
+
+//     // Only change code above this line
+//     return answer;
+// }
+
+// Generate random number between 1 and 10
+
