ec627942dfebda9127c7fd7bf155ffb5d316c780
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2021-11-04 00:32:13 +0600

    testing

--- app.js
+++ app.js
@@ -97,3 +97,97 @@
 
 // Generate random number between 1 and 10
 
+// function checkObj(obj, checkProp) {
+//     // Only change code below this line
+//     return obj.hasOwnPoperty(checkProp) ? obj[checkProp] : 'Not Found';
+//     // Only change code above this line
+// }
+// const res = checkObj({ gift: 'pony', pet: 'kitten', bed: 'sleigh' }, 'gift');
+// console.log(res)
+// Setup
+// Setup
+// const recordCollection = {
+//     2548: {
+//         albumTitle: 'Slippery When Wet',
+//         artist: 'Bon Jovi',
+//         tracks: ['Let It Rock', 'You Give Love a Bad Name'],,
+//     },
+//     2468: {
+//         albumTitle: '1999',
+//         artist: 'Prince',
+//         tracks: ['1999', 'Little Red Corvette'],,
+//     },
+//     1245: {
+//         artist: 'Robert Palmer',
+//         tracks: [],,
+//     },
+//     5439: {
+//         albumTitle: 'ABBA Gold',,
+//     },
+// };
+
+// // Only change code below this line
+// function updateRecords(records, id, prop, value) {
+//     if (prop != 'tracks' && value != '') {
+//         records[id].prop = value;;
+//     } else if (prop ===  'tracks' && records[id].hasOwnProperty('tracks') === false) {
+//         records[id].prop = value;;
+//     } else if (prop === 'tracks' && value !== '') {
+//         records[id].prop.push (value);
+//     } else if (value === '') {
+//         delete records[id].prop;;
+//     }
+//     return records;
+// }
+
+// // updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+// const myArray = [];
+
+// // Only change code below this line
+// for (i = 9; i > 0; i -= 2) {
+//     myArray.push(i);
+// }
+
+// Setup
+const contacts = [
+    {
+        firstName: 'Akira',
+        lastName: 'Laine',
+        number: '0543236543',
+        likes: ['Pizza', 'Coding', 'Brownie Points'],
+    },
+    {
+        firstName: 'Harry',
+        lastName: 'Potter',
+        number: '0994372684',
+        likes: ['Hogwarts', 'Magic', 'Hagrid'],
+    },
+    {
+        firstName: 'Sherlock',
+        lastName: 'Holmes',
+        number: '0487345643',
+        likes: ['Intriguing Cases', 'Violin'],
+    },
+    {
+        firstName: 'Kristian',
+        lastName: 'Vos',
+        number: 'unknown',
+        likes: ['JavaScript', 'Gaming', 'Foxes'],
+    },
+];
+
+function lookUpProfile(name, prop) {
+    // Only change code below this line
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].firstName === name) {
+            if (contacts[i].hasOwnProperty(prop)) {
+                return contacts[i][prop];
+            }
+            return 'No such property';
+        }
+    }
+    return 'No such contact';
+    // Only change code above this line
+}
+
+lookUpProfile('Akira', 'likes');
